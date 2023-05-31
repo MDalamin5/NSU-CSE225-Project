b3fed77fccc8dd93f9e56c5dc0bbf727916fedc0
MDalamin5 <mdal.amin5@northsouth.edu>
2023-05-31 12:47:33 +0600

    Delete Function Update - Al Amin

--- SrcCode/main.cpp
+++ SrcCode/main.cpp
@@ -66,7 +66,7 @@ public:
         newNode->next = head;
         head = newNode;
         size++;
-        cout << endl<<size<<"<< Faculty Sussfully add" << endl;
+        cout << endl<<size<<" Faculty Sussfully add" << endl;
     }
     //Read data or Searching 
     void readFacultyInfo()
@@ -174,6 +174,180 @@ void searchByPhoneNo()
             cout<<"Sorry! Name Not Found"<<endl;
         }
     }
+// delete Faculty Info
+
+    void DeleteFacultyInfo()
+    {
+        int cos_opt;
+        cout<<"which contact you want to delete"<<endl;
+        //cout<<"Enter contact name"<<endl;
+        cout<<"How way to daelte the contact"<<endl;
+        cout<<"Chose your option: "<<endl;
+        cout<<"1. Delete faculty Search by name: "<<endl;
+        cout<<"2. Delete Faculty Search by PhoneNumber: "<<endl;
+        cout<<"Please Enter your choise: "<<endl;
+        cin>>cos_opt;
+        switch (cos_opt)
+        {
+            case 1: 
+            eraseSrcByName();
+            break;
+
+            case 2:
+            eraseSrcByPhoneNumber();
+            break;
+        
+            default:
+            cout<<"You are enteing wrong input"<<endl;
+            break;
+        }
+
+    }
+    void eraseFromHead()
+    {
+        facultyNode *temp=head;
+        head=temp->next;
+        size--;
+        delete temp;
+        cout<<"Delete "<<head->name <<"Faculty Info From Our List"<<endl;
+        //return;
+    }
+     void eraseSrcByName()
+    {
+         string src_name;
+        cout << "Please Enter the name: ";
+        fflush(stdin);
+        getline(cin, src_name);
+
+    if(size==0)
+    {
+        cout<<"No data in your file"<<endl;
+        return;
+    }
+
+        int cur_indx=0;
+        facultyNode *a = head;
+        bool flag;
+        while (a != NULL)
+        {
+            flag = true;
+            for (int i = 0; i < src_name.length(); i++)
+            {
+                if (src_name[i] != a->name[i])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                break;
+            }
+            a = a->next;
+            cur_indx++;
+        }
+        cout<<cur_indx<<endl;
+        if (flag)
+        {
+            cout<<endl;
+            //cout << "Name Found " << endl;
+        }
+        else
+        {
+            cout << "Sorry! Name Not Found";
+            return;
+        }
+
+        if(cur_indx==0)
+        {
+            eraseFromHead();
+            return;
+        }
+        int count_index=0;
+        facultyNode *prev=head;
+        while(count_index!=cur_indx-1)
+        {
+            prev=prev->next;
+            count_index++;
+        }
+
+            prev->next=a->next;
+            size--;
+            cout<<"Delete "<<a->name <<"Faculty Info From Our List"<<endl;
+            delete a;
+            
+    } 
+
+   //delete by phonenumber
+    void eraseSrcByPhoneNumber()
+    {
+        string src_PhNO;
+        cout << "Please Enter the Phone_Number: ";
+        fflush(stdin);
+        getline(cin, src_PhNO);
+
+    if(size==0)
+    {
+        cout<<"No data in your file"<<endl;
+        return;
+    }
+
+        int cur_indx=0;
+        facultyNode *a = head;
+        bool flag;
+        while (a != NULL)
+        {
+            flag = true;
+            for (int i = 0; i < src_PhNO.length(); i++)
+            {
+                if (src_PhNO[i] != a->mobileNumber[i])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                break;
+            }
+            a = a->next;
+            cur_indx++;
+        }
+        cout<<cur_indx<<endl;
+        if (flag)
+        {
+            cout<<endl;
+            //cout << "Name Found " << endl;
+        }
+        else
+        {
+            cout << "Sorry! Name Not Found";
+            return;
+        }
+
+        if(cur_indx==0)
+        {
+            eraseFromHead();
+            return;
+        }
+        int count_index=0;
+        facultyNode *prev=head;
+        while(count_index!=cur_indx-1)
+        {
+            prev=prev->next;
+        }
+
+            prev->next=a->next;
+            size--;
+            cout<<"Delete "<<a->name <<"Faculty Info From Our List"<<endl;
+            delete a;
+    }
+    // delete method end
+
+    // Faculty Modify Function
+
+
+
 
 //display data
     void travers()
@@ -219,7 +393,8 @@ class manageRecord
 
             case 2:
             //c.insertCourse();
-            fac.readFacultyInfo();
+            //fac.readFacultyInfo();
+            fac.DeleteFacultyInfo();
             break;
 
             case 3:
@@ -273,7 +448,37 @@ class manageRecord
     }
     void updateRecord()
     {
-        cout<<"Update"<<endl;
+        int searchInput;
+       cout<<"\n"<<"Please Chose Your Option"<<endl;
+       cout<<"1. Delete from Facuty Data"<<endl;
+       cout<<"2. delete Course Data"<<endl;
+       cout<<"3. delete Course Assignment"<<endl;
+       cout<<"0. Go to home page"<<endl;
+       cout<<endl<<"Please Enter Your Choise: ";
+       cin>>searchInput;
+       switch(searchInput)
+       {
+        case 0:
+        TempMain();
+        break;
+
+        case 1:
+        fac.DeleteFacultyInfo();
+        break;
+
+        case 2:
+        //c.DeleteCourseINfo();
+        fac.travers();
+        break;
+
+        case 3:
+        //fac.searchByName();
+        break;
+
+        default:
+        cout<<"You are entering invalid input\n\n Try again \n\n";
+        seacrchigRecord();
+    }
     }
     void genrateReport()
     {
